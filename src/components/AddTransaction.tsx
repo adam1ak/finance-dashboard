64619a64c4079c2c02e934134c2dbd38d678808d
adam1ak <f.admiakk@gmail.com>
2025-06-29 13:43:02 +0200

    apply modern Tailwind styling to add transaction form

--- src/components/AddTransaction.tsx
+++ src/components/AddTransaction.tsx
@@ -1,6 +1,10 @@
 import { useForm, Controller } from 'react-hook-form';
 import DatePicker from 'react-datepicker';
+
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
+
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
 
 type FormData = {
     title: string;
@@ -10,6 +14,11 @@ type FormData = {
 };
 
 function AddTransaction() {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    // true = income, false = expense
+    const [transactionType, setTransactionType] = useState(true)
 
     const {
         register,
@@ -32,8 +41,14 @@ function AddTransaction() {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="title">Title</label>
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="
+                flex flex-col w-3/4 h-full
+                py-2 px-4
+            ">
+            <h1 className="text-2xl text-neutral-900 font-bold">Add Transaction</h1>
+            <label htmlFor="title" className="mt-3 text-xl text-neutral-600 font-medium">Title</label>
             <input
                 id="title"
                 type="text"
@@ -51,39 +66,76 @@ function AddTransaction() {
                             value: 50,
                             message: "Title cannot exceed 50 characters"
                         }
-                    })} />
-            {errors.title && <p>{errors.title.message}</p>}
+                    })}
+                className="
+                    w-full border-2 border-stone-200 rounded-sm py-1.5 pl-2 focus:outline-none
+                "/>
+            {errors.title && <p className="text-sm ml-2 text-red-700 font-medium">{errors.title.message}</p>}
 
-            <label htmlFor="amount">Amount</label>                    
-            <input
-                id="amount"
-                type="number"
-                placeholder="24$"
-                {...register("amount",
-                    {
-                        required: "Amount is required",
-                        validate : (value) => value !== 0 || "Amount cannot be zero",
-                        min: {
-                            value: -1000000,
-                            message: "Amount must be ≥ -1,000,000"
-                        },
-                        max: {
-                            value: 1000000,
-                            message: "Amount must be ≤ 1,000,000"
-                        }
-                    })} />
-            {errors.amount && <p>{errors.amount.message}</p>}
+            <label htmlFor="amount" className="mt-4 text-xl text-neutral-600 font-medium">Amount</label>
+            <div className="relative">
+                <p 
+                onClick={() => setTransactionType((prev) => !prev)}
+                className={`
+                    absolute top-1/2 -translate-y-1/2 ml-2
+                    w-6 rounded-lg text-center
+                    text-white font-bold
+                    cursor-pointer
+                    ${transactionType ? "bg-green-600" : "bg-red-700"}
+                `}>{transactionType ? "I" : "E"}</p>
+                <input
+                    id="amount"
+                    type="number"
+                    placeholder="0"
+                    step={0.01}
+                    {...register("amount",
+                        {
+                            required: "Amount is required",
+                            validate: (value) => value !== 0 || "Amount cannot be zero",
+                            min: {
+                                value: -1000000,
+                                message: "Amount must be ≥ -1,000,000"
+                            },
+                            max: {
+                                value: 1000000,
+                                message: "Amount must be ≤ 1,000,000"
+                            }
+                        })}
+                    className="
+                    w-full border-2 border-stone-200 rounded-sm py-1.5 pl-10 focus:outline-none
+                " />
+            </div>
 
-            <label htmlFor="category">Category</label>
-            <select id="category" {...register("category", {required : "Category is required"})}>
-                <option value="Food">Food</option>
-                <option value="Transport">Transport</option>
-                <option value="Education">Education</option>
-                <option value="Other">Other</option>
-            </select>
-            {errors.category && <p>{errors.category.message}</p>}
+            {errors.amount && <p className="text-sm ml-2 text-red-700 font-medium">{errors.amount.message}</p>}
 
-            <label htmlFor="date">Date</label>
+            <label htmlFor="category" className="mt-4 text-xl text-neutral-600 font-medium">Category</label>
+            <div className="relative">
+                <select
+                    id="category"
+                    {...register("category", { required: "Category is required" })}
+                    onClick={() => setIsOpen((prev) => !prev)}
+                    onBlur={() => setIsOpen((prev) => !prev)}
+                    className="
+                        w-full border-2 border-stone-200 rounded-sm py-1.5 pl-2 appearance-none focus:outline-none
+                    ">
+                    <option value="" disabled hidden>Select a category</option> {/* Placeholder */}
+                    <option value="Food">Food</option>
+                    <option value="Transport">Transport</option>
+                    <option value="Education">Education</option>
+                    <option value="Other">Other</option>
+                </select>
+
+                <div className="pointer-events-none absolute top-1/2 -translate-y-1/2 right-0 mr-3">
+                    <ChevronDownIcon
+                        className={`
+                            size-4 transition-all duration-400 ease-in-out  
+                            ${isOpen ? "rotate-180" : "rotate-360"}`
+                        } />
+                </div>
+            </div>
+            {errors.category && <p className="text-sm ml-2 text-red-700 font-medium">{errors.category.message}</p>}
+
+            <label htmlFor="date" className="mt-4 text-xl text-neutral-600 font-medium">Date</label>
             <Controller
                 name="date"
                 control={control}
@@ -94,12 +146,20 @@ function AddTransaction() {
                         selected={field.value}
                         onChange={(date) => field.onChange(date)}
                         onBlur={field.onBlur}
-                        dateFormat="dd/MM/yyyy" />
+                        dateFormat="dd/MM/yyyy"
+                        className="
+                    w-full border-2 border-stone-200 rounded-sm py-1.5 pl-2 focus:outline-none
+                "  />
                 )}
             />
-            {errors.date && <p>{errors.date.message}</p>}
+            {errors.date && <p className="text-sm ml-2 text-red-700 font-medium">{errors.date.message}</p>}
 
-            <input type="submit" value="Add transactions"/>
+            <input
+                type="submit"
+                value="Add transactions"
+                className="
+                    mt-4 w-full py-2 rounded-md bg-blue-600 text-white font-semibold 
+                " />
         </form>
     )
 }
